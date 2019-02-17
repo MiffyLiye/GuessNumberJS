36403c1a7d444740a648cc2ac810eb2d0bdddee5
Wang, Tao <miffyliye@gmail.com>
2019-02-17 18:18:02 +0800

    1A0B

--- src/guess-feedback.js
+++ src/guess-feedback.js
@@ -1,4 +1,10 @@
-function getGuessFeedback() {
+function getGuessFeedback(secretNumber, guessedNumber) {
+    for (let i = 0; i < 4; i++) {
+        if (secretNumber[i] === guessedNumber[i]) {
+            return '1A0B';
+        }
+    }
+
     return '0A0B';
 }
 
--- test/guess-feedback.test.js
+++ test/guess-feedback.test.js
@@ -9,4 +9,40 @@ describe('number feedback', () => {
 
         expect(feedback).toBe('0A0B');
     })
+
+    it('should return 1A0B when secret number is 1234 and guessed number is 1678', () => {
+        const secretNumber = '1234';
+        const guessedNumber = '1678';
+
+        const feedback = getGuessFeedback(secretNumber, guessedNumber);
+
+        expect(feedback).toBe('1A0B');
+    })
+
+    it('should return 1A0B when secret number is 1234 and guessed number is 5278', () => {
+        const secretNumber = '1234';
+        const guessedNumber = '5278';
+
+        const feedback = getGuessFeedback(secretNumber, guessedNumber);
+
+        expect(feedback).toBe('1A0B');
+    })
+
+    it('should return 1A0B when secret number is 1234 and guessed number is 5638', () => {
+        const secretNumber = '1234';
+        const guessedNumber = '5638';
+
+        const feedback = getGuessFeedback(secretNumber, guessedNumber);
+
+        expect(feedback).toBe('1A0B');
+    })
+
+    it('should return 1A0B when secret number is 1234 and guessed number is 5674', () => {
+        const secretNumber = '1234';
+        const guessedNumber = '5674';
+
+        const feedback = getGuessFeedback(secretNumber, guessedNumber);
+
+        expect(feedback).toBe('1A0B');
+    })
 });
