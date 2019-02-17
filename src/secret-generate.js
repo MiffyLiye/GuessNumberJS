9cbe21c1e369fa15d14c206ca717c824d1b1f84d
Wang, Tao <miffyliye@gmail.com>
2019-02-17 20:32:42 +0800

    test with stub

--- src/secret-generate.js
+++ src/secret-generate.js
@@ -1,5 +1,19 @@
-function generateSecretNumber() {
-    return '0000';
+function defaultGetRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function generateSecretNumber(customGetRandomInt) {
+    const getRandomInt = customGetRandomInt || defaultGetRandomInt;
+
+    let secret = '';
+    while (secret.length < 4) {
+        const newDigit = getRandomInt(10);
+        if (!secret.includes(newDigit)) {
+            secret = secret + newDigit;
+        }
+    }
+
+    return secret;
 }
 
 module.exports = { generateSecretNumber };
--- test/secret-generate.test.js
+++ test/secret-generate.test.js
@@ -6,4 +6,29 @@ describe('secret generator', () => {
 
         expect(secretNumber).toMatch(/^\d{4}$/);
     });
+
+    it('should generate unique digits', () => {
+        const secretNumber = generateSecretNumber();
+
+        const uniqueDigits = new Set(secretNumber.split(''));
+        expect(uniqueDigits.size).toBe(secretNumber.length);
+    });
+
+    it.each`
+    secretNumber    |   sequence
+    ${'1234'}       |   ${[1, 2, 3, 4]}
+    ${'4321'}       |   ${[4, 3, 2, 1]}
+    ${'1234'}       |   ${[1, 2, 2, 3, 4]}
+    `('should generate $secretNumber when random int generator gives $sequence', ({secretNumber, sequence}) => {
+        let position = 0;
+        const customGenerateInt = () => {
+            const digit = sequence[position];
+            position += 1;
+            return digit;
+        };
+
+        const actualSecretNumber = generateSecretNumber(customGenerateInt);
+
+        expect(actualSecretNumber).toBe(secretNumber)
+    })
 });
