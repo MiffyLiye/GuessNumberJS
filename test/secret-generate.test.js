bb09a32ccc057007114085c8ca618f1439f21506
Wang, Tao <miffyliye@gmail.com>
2019-03-01 14:05:25 +0800

    remove tests about implementation details

--- test/secret-generate.test.js
+++ test/secret-generate.test.js
@@ -14,20 +14,4 @@ describe('secret generator', () => {
         const uniqueDigits = new Set(secretNumber.split(''));
         expect(uniqueDigits.size).toBe(secretNumber.length);
     });
-
-    it.skip.each`
-    secretNumber    |   sequence
-    ${'1234'}       |   ${[1, 2, 3, 4]}
-    ${'4321'}       |   ${[4, 3, 2, 1]}
-    ${'1234'}       |   ${[1, 2, 2, 3, 4]}
-    `('should generate $secretNumber when random int generator gives $sequence', ({secretNumber, sequence}) => {
-        const customGenerateInt = sinon.stub();
-        for (let i = 0; i < sequence.length; i += 1) {
-            customGenerateInt.onCall(i).returns(sequence[i]);
-        }
-
-        const actualSecretNumber = generateSecretNumber(customGenerateInt);
-
-        expect(actualSecretNumber).toBe(secretNumber)
-    })
 });
